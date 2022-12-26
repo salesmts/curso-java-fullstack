c86db782778725ffedf73fbe98207e532b96bbd6
Matheus Sales <sales.matheus.sousa10@gmail.com>
2022-12-26 23:49:50 +0000

    testing revert commit

--- src/main/java/org/sales/Main.java
+++ src/main/java/org/sales/Main.java
@@ -2,7 +2,7 @@ package org.sales;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
+        System.out.println("Testing commits");
     }
 }
